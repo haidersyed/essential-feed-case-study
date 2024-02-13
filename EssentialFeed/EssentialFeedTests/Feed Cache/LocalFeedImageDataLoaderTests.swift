54c716dfaee5e3ee2bf0ea6fb39b3928d67ef9ef
haidersyed <haiderrazvi@hotmail.com>
2024-02-13 07:24:49 +0500

    LocalFeedImageDataLoader.loadImageData requests stored data for URL from `FeedImageDataStore`

--- EssentialFeed/EssentialFeedTests/Feed Cache/LocalFeedImageDataLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/LocalFeedImageDataLoaderTests.swift
@@ -5,9 +5,28 @@
 import XCTest
 import EssentialFeed
 
-final class LocalFeedImageDataLoader {
-	init(store: Any) {
+protocol FeedImageDataStore {
+	func retrieve(dataForURL url: URL)
+}
 
+final class LocalFeedImageDataLoader : FeedImageDataStore {
+	func retrieve(dataForURL url: URL) {
+		
+	}
+	
+	private struct Task: FeedImageDataLoaderTask {
+		func cancel() {}
+	}
+
+	private let store: FeedImageDataStore
+
+	init(store: FeedImageDataStore) {
+		self.store = store
+	}
+
+	func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
+		store.retrieve(dataForURL: url)
+		return Task()
 	}
 }
 
@@ -20,17 +39,25 @@ class LocalFeedImageDataLoaderTests: XCTestCase {
 	}
 
 	// MARK: - Helpers
-
-	private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #file, line: UInt = #line) -> (sut: LocalFeedImageDataLoader, store: FeedStoreSpy) {
-		let store = FeedStoreSpy()
+	
+	private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #file, line: UInt = #line) -> (sut: LocalFeedImageDataLoader, store: StoreSpy) {
+		let store = StoreSpy()
 		let sut = LocalFeedImageDataLoader(store: store)
 		trackForMemoryLeaks(store, file: file, line: line)
 		trackForMemoryLeaks(sut, file: file, line: line)
 		return (sut, store)
 	}
 
-	private class FeedStoreSpy {
-		let receivedMessages = [Any]()
+	private class StoreSpy:FeedImageDataStore {
+		enum Message: Equatable {
+			case retrieve(dataFor: URL)
+		}
+
+		private(set) var receivedMessages = [Message]()
+
+		func retrieve(dataForURL url: URL) {
+			receivedMessages.append(.retrieve(dataFor: url))
+		}
 	}
 
 }
